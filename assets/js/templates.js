b6a334b876272f7ac4829278fdda799f487a3718
I_SER_I <I_SER_I@hotmail.com>
2021-12-06 19:41:39 +0300

    feat: add header/footer autoloader

---
+++ assets/js/templates.js
@@ -0,0 +1,33 @@
+class MyFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <footer class="footer">
+                <p class="footer__copyright">
+                    &copy 2021 Sergei Papikian
+                </p>
+            </footer>
+        `
+    }
+}
+
+customElements.define('i_ser_i-footer', MyFooter)
+
+class MyHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <header>
+                <nav>
+                    <ul class="nav">
+                        <li class="nav__item"><a class="nav__link " href="index.html">Start</a></li>
+                        <li class="nav__item"><a class="nav__link " href="profile.html">Profile</a></li>
+                        <li class="nav__item"><a class="nav__link " href="skills.html">Skills</a></li>
+                        <li class="nav__item"><a class="nav__link " href="education.html">Education</a></li>
+                        <li class="nav__item"><a class="nav__link " href="contact.html">Contact</a></li>
+                    </ul>
+                </nav>
+            </header>
+        `
+    }
+}
+
+customElements.define('i_ser_i-header', MyHeader)
